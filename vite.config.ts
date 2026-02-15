d6c4a5274598d11c0708c7b350d469c29b7191dd
saklsalk <michlle25@gmail.com>
2026-02-15 15:56:21 +0100

    Fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
   },
   plugins: [react()],
-  base: '/Florentia-Fracta/',
+  base: '/CittaFracta/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
